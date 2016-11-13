e323f79e74c8308866b24c0b5cf9b1733acdb119
gvp-raviteja <ganireddy.raviteja@gmail.com>
2016-11-13 00:54:23 -0500

    added apa ai http call

--- new/www/js/controllers.js
+++ new/www/js/controllers.js
@@ -1,11 +1,15 @@
 angular.module('starter.controllers', ['ionic'])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope,$http) {
   $scope.data = {
     speechText: ''
   };
   $scope.recognizedText = '';
- 
+  var accessToken = "7853febc373644728bc5d5f9e6d0282d";
+  var subscriptionKey = "4ff074a867ab4d71a8f12eb5fd20822d";
+  var baseUrl = "https://api.api.ai/v1/";
+  
+
   $scope.speakText = function() {
     TTS.speak({
            text: $scope.recognizedText,
@@ -19,12 +23,29 @@ angular.module('starter.controllers', ['ionic'])
   };
  
   $scope.record = function() {
-    var recognition = new webkitSpeechRecognition();
+    var recognition = new SpeechRecognition();
     recognition.onresult = function(event) {
       console.log(event);
         if (event.results.length > 0) {
             $scope.recognizedText = event.results[0][0].transcript;
             $scope.$apply()
+            $http({
+              method: "POST",
+              url: baseUrl + "query/",
+              contentType: "application/json; charset=utf-8",
+              dataType: "json",
+              headers: {
+                "Authorization": "Bearer " + accessToken,
+                "ocp-apim-subscription-key": subscriptionKey
+              },
+              data: JSON.stringify({ q: $scope.recognizedText, lang: "en" }),
+            }).then(function successCallback(response) {
+                console.log(response.data.result.speech);
+                $scope.recognizedText = response.data.result.speech;
+              }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+              });
         }
     };
     recognition.start();

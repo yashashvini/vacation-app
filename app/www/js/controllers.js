3095f5213aec56341274ba2eafa37eec9f122e2b
yash_pakala <yashashvinipakala@gmail.com>
2017-07-15 15:11:46 -0500

    initial

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -5,8 +5,8 @@ angular.module('starter.controllers', ['ionic'])
     speechText: ''
   };
   $scope.recognizedText = [];
-  var accessToken = "8bd3b6024a8e461f8e4e63c181882295";
-  var baseUrl = "https://cookingasst.herokuapp.com/";
+  var accessToken = "1e4bd06b41b745b9b49c47318de85bb2";
+  var baseUrl = "https://api.api.ai/v1/";
   $scope.text = "Tap here and say Hello";
   
   $ionicModal.fromTemplateUrl('my-modal.html', {
@@ -17,22 +17,23 @@ angular.module('starter.controllers', ['ionic'])
    });
 
   $scope.speakText = function() {
-    window.TTS.speak({
-           text: $scope.recognizedText[0].text,
-           locale: 'en-GB',
-           rate: 1.5
-       }, function (success) {
-            //$scope.record();
-           // Do Something after success
-       }, function (reason) {
-          //alert('some error occurred, speak again');
-          //$scope.record();
-          // Handle the error case
-       });
+    window.speechSynthesis.speak($scope.msg);
+    // window.TTS.speak({
+    //        text: $scope.recognizedText[0].text,
+    //        locale: 'en-GB',
+    //        rate: 1.5
+    //    }, function (success) {
+    //         //$scope.record();
+    //        // Do Something after success
+    //    }, function (reason) {
+    //       //alert('some error occurred, speak again');
+    //       //$scope.record();
+    //       // Handle the error case
+    //    });
   };
 
   $scope.record = function(signal) {
-    var recognition = new SpeechRecognition();
+    var recognition = new webkitSpeechRecognition();
     //recognition.continuous = true;
     //recognition.interimResults = true;
     console.log('calling record');
@@ -48,21 +49,25 @@ angular.module('starter.controllers', ['ionic'])
         $scope.$apply();
         $http({
           method: "POST",
-          url: baseUrl,
+          url: baseUrl + "query/?v=20170715&amp;",
           contentType: "application/json; charset=utf-8",
           dataType: "json",
           headers: {
             "Authorization": "Bearer " + accessToken
           },
-          data: $scope.recognizedText[0].text
+
+          data: JSON.stringify({ query: $scope.recognizedText[0].text, lang: "en", sessionId: "873fdf8a-aae9-41af-a453-8450622d578d",timezone:'2017-07-15T11:03:10-0500' }),
         }).then(function successCallback(response) {
           var displayObjServer = {
             text : '',
             origin : 'system'
           };
-          displayObjServer.text = response.data;
+          displayObjServer.text = response.data.result.fulfillment.speech;
+          var msg = displayObjServer.text;
+          var utterThis = new SpeechSynthesisUtterance(msg);
           $scope.recognizedText.unshift(displayObjServer);
-            $scope.speakText();
+          window.speechSynthesis.speak(utterThis);
+            //$scope.speakText();
           }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
--- servercode/main.py
+++ servercode/main.py
@@ -4,43 +4,62 @@ app = Flask(__name__)
 import os.path
 import sys
 import json
-try:
-    import apiai
-except ImportError:
-    sys.path.append(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
-    )
-    import apiai
-# CLIENT_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
-client_access_token = '8bd3b6024a8e461f8e4e63c181882295'
+import datetime
+# try:
+#     import apiai
+# except ImportError:
+#     sys.path.append(
+#         os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+#     )
+#     import apiai
+# # CLIENT_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+# client_access_token = '8bd3b6024a8e461f8e4e63c181882295'
 
 
-#@app.route("/")
+@app.route("/")
 def basic():
-    ai = apiai.ApiAI(client_access_token)
-    request = ai.text_request()
-    # request.lang = 'en'  # optional, default value equal 'en'
-    # request.session_id = "unique"
-    request.query = "Chicken"
-    response = request.getresponse()
-    #print json.loads(response.read())
-    #output = json.loads(response.read())['result']["fulfillment"]["messages"][0]['speech']
-    #print json.loads(response.read())['result']['fulfillment']
-    output = json.loads(response.read())['result']
-    output_speech = output["fulfillment"]["speech"]
-    intent_name = output["metadata"]["intentName"]
-    if (output_speech.rsplit(None,1)[-1] == 'chicken:'):
-        ai = apiai.ApiAI("2014eca4faff41eaa164b46c965aaa52")
-        request = ai.text_request()
-        request.query = "yes"
-        response = request.getresponse()
-        output = json.loads(response.read())['result']
-        output_speech = output["fulfillment"]["speech"]
-        print output_speech
-        intent_name = output["metadata"]["intentName"]
-    print output_speech,intent_name
-    return response.read()
+    long_weekends = {"Independence Day": "TUESDAY, JULY,2017",
+    "LABOR DAY":"MONDAY, SEPTEMBER 4, 2017 ",
+    "COLUMBUS DAY":"MONDAY, OCTOBER 9, 2017",
+    "VETERANS DAY":"FRIDAY, NOVEMBER 10, 2017",
+    "THANKSGIVING DAY":"THURSDAY, NOVEMBER 23, 2017",
+    "MONDAY, DECEMBER 25, 2017":"CHRISTMAS DAY"}
+    vacations_by_month = {"January": ["Caribbean","Australia","Shetland Islands","Scotland","Northern lights in Norway",
+                                     "Mexico (Puerto Escondido for something lively, Mazunte, San Agustinillo and Zipolite for something a little quieter)"," Brazil",
+                                     " Ethiopia", "Scotland", "Norway", "France", "Switzerland", "Austria", "The West Indies and the Caribbean", "the Dominican Republic", "South Africa"],
+                          "February": ["Buenos Aires", "Argentina", "Africa", "Egypt", "South Africa", "Hawaii"],
+                          "March" : ["Italy", "Asia", "America", "Cuba", "Argentina", "the Maldives"],
+                          "April" : ["Crete", "Malta", "Baleric Islands", "Africa", "Amsterdam, America, Turkey, the Philippines, Japan, Australia."],
+                          "May" : ["Europe,the Philippines, the Bahamas, Mexico, Australia"],
+                          "June" : ["Europe", "Namibia", "Brazil", "The Caribbean", "Canada", "Cuba"],
+                          "July" : ["Australia", "Europe", "Indonesia", "Peru", "Bolivia", "America"],
+                          "August" : ["Europe", "South Africa", "Peru and Tibet"],
+                          "September" : ["Portugal", "Croatia", "Africa", "America", "China", "Japan"],
+                          "October" : ["India", "Calcutta", "Turkey", "Cyprus", "Africa", "Egypt", "South Africa", "America", "Argentina", "Japan", "Venice"],
+                          "November" : ["New Zealand", "Hawaii", "Aspen", "Germany", "America", "South America", "Morocco", "Belize", "Hong Kong"],
+                          "December" : ["Mexico", "Belize", "Skiing Destinations", "the Caribbean", "Cambodia", "Australia", "Iceland"]
+                          }
+    current_date = str(datetime.datetime.now())
+    month_list = ["January","February","March","April","May","June","July","September","October","November","December"]
+    if(current_date[5:6] == '0'):
+        month = current_date[6:7]
+    else:
+        month = current_date[5:7]
+    month = month_list[int(month) - 1]
+    print month
+    holiday_occasion = "None"
+    holiday_date = "None"
+    for key, value in long_weekends.iteritems():
+        if(month.upper() in value ):
+            holiday_occasion = key
+            holiday_date = value
+            break
+    vacation = vacations_by_month[month]
+    return [holiday_occasion,holiday_date,vacation]
+
+
+
 
 if __name__ == '__main__':
     basic()
-    #app.run()
+    app.run()
